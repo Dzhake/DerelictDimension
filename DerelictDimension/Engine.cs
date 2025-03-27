8dbb59f2c0c6649ef269da081c53510501c5c53f
Dzhake <andryzindmi@gmail.com>
2025-03-27 14:53:59 +0300

    New day, new repo!

---
+++ DerelictDimension/Engine.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoPlus.Graphics;
+using System.Globalization;
+using System.Threading.Tasks;
+using MonoPlus.Assets;
+using MonoPlus.Input;
+
+namespace DerelictDimension;
+
+public class Engine : Game
+{
+    public static Engine? Instance;
+    public static AssetManager? MainAssetManager;
+    public static Texture2D? idk;
+
+    public Engine()
+    {
+        CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+        CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
+
+        Renderer.OnGameCreated(this);
+        Instance = this;
+        IsFixedTimeStep = false;
+    }
+
+    protected override void Initialize()
+    {
+        Renderer.Initialize(this);
+        Input.Initialize(this);
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        MainAssetManager = new FileSystemAssetManager($"{AppContext.BaseDirectory}Content/");
+        Assets.RegisterAssetManager(MainAssetManager, "vanilla");
+        Renderer.spriteBatch = new SpriteBatch(GraphicsDevice);
+        Task.Run(LoadContentAsync);
+    }
+
+    protected async void LoadContentAsync()
+    {
+        idk = await Assets.LoadAsync<Texture2D>("vanilla:/idk");
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        base.Update(gameTime);
+        Input.Update();
+
+
+        Input.PostUpdate();
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.Black);
+        Renderer.Begin();
+        if (idk is not null) Renderer.DrawTexture(idk, Vector2.Zero, Color.White);
+        Renderer.End();
+        base.Draw(gameTime);
+    }
+}
---
+++ DerelictDimension/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace DerelictDimension;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        RunGame();
+    }
+
+    public static void RunGame()
+    {
+        new Engine().Run();
+    }
+}
+

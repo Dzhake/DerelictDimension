e5105b1d26d2d937d55ca60775aa82831703838c
Dzhake <andryzindmi@gmail.com>
2025-03-30 14:10:02 +0300

    Finish assets preloading, finish effects compiling with mgfxc

--- DerelictDimension/Engine.cs
+++ DerelictDimension/Engine.cs
@@ -4,6 +4,7 @@ using Microsoft.Xna.Framework.Graphics;
 using MonoPlus.Graphics;
 using System.Globalization;
 using System.IO;
+using System.Threading.Tasks;
 using MonoPlus.Assets;
 using MonoPlus.Input;
 using MonoPlus.Time;
@@ -14,7 +15,7 @@ public class Engine : Game
 {
     public static Engine? Instance;
     public static AssetManager? MainAssetManager;
-    public static Texture2D? idk;
+    public Effect? effect;
 
     public Engine()
     {
@@ -35,13 +36,14 @@ public class Engine : Game
 
     protected override void LoadContent()
     {
+        Renderer.spriteBatch = new SpriteBatch(GraphicsDevice);
+
         string contentArchivePath = $"{AppContext.BaseDirectory}Content.zip";
         if (File.Exists(contentArchivePath))
             MainAssetManager = new ZipArchiveAssetManager(contentArchivePath);
         else
             MainAssetManager = new FileSystemAssetManager($"{AppContext.BaseDirectory}Content/");
         Assets.RegisterAssetManager(MainAssetManager, "vanilla");
-        Renderer.spriteBatch = new SpriteBatch(GraphicsDevice);
         MainAssetManager.PreloadAssets();
     }
 
@@ -59,8 +61,8 @@ public class Engine : Game
     {
         if (GraphicsSettings.PauseOnFocusLoss && !IsActive) return;
         GraphicsDevice.Clear(Color.Black);
-        Renderer.Begin();
-        if (idk is not null) Renderer.DrawTexture(idk, Vector2.Zero, Color.White);
+        Renderer.Begin(SpriteSortMode.Immediate, effect: effect);
+        Renderer.DrawRect(new(0,0), new(500, 500), Color.DarkBlue);
         Renderer.End();
         base.Draw(gameTime);
     }

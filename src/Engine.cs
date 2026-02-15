49c4ccaf81cb75d0da527d75d9c58f85f79fe8c1
Dzhake <andryzindmi@gmail.com>
2026-02-15 19:35:09 +0300

    Add MLEM

--- src/Engine.cs
+++ src/Engine.cs
@@ -1,17 +1,18 @@
-﻿using Microsoft.Xna.Framework;
+﻿using FontStashSharp;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using MLEM.Extended.Font;
+using MLEM.Font;
+using MLEM.Formatting;
 using Monod;
 using Monod.AssetsModule;
 using Monod.AssetsModule.AssetLoaders;
 using Monod.Graphics;
 using Monod.Graphics.Fonts;
-using Monod.Graphics.Fonts.Bitmap;
 using Monod.InputModule;
 using Monod.InputModule.InputActions;
 using Monod.InputModule.Parsing;
-using Monod.Shared;
 using System;
-using System.Text.Json;
 
 namespace DerelictDimension;
 
@@ -33,6 +34,8 @@ public class Engine : MonodGame
 
     public InputAction? Action;
 
+    public TokenizedString tokenized;
+
     /// <summary>
     /// Creates a new <see cref="Engine"/>.
     /// </summary>
@@ -47,24 +50,32 @@ public class Engine : MonodGame
 
         string contentPath = $"{AppContext.BaseDirectory}Content";
         MainAssetManager = new AssetManager(new FileAssetLoader((contentPath)));
+
         Assets.RegisterAssetManager(MainAssetManager, "");
-        MainAssetManager.LoadAsset("THEFONT.png");
-        MainAssetManager.LoadAsset("THEFONT_info.json");
+        MainAssetManager.LoadAsset("Fonts/monogram-extended.ttf");
+        MainAssetManager.LoadAsset("Fonts/monogram-extended-italic.ttf");
         LoadFont();
         Assets.OnReload += LoadFont;
         MainAssetManager.LoadAssets();
 
-        Action = new OrAction([new HeldAction(Key.D1), new AndAction([new PressedAction(Key.D2), new DownAction(Key.LeftControl)])]);
+        //Action = new OrAction([new HeldAction(Key.D1), new AndAction([new PressedAction(Key.D2), new DownAction(Key.LeftControl)])]);
         Action = InputActionParser.TryParse(textbox);
-        foreach (var error in InputActionParser.Errors) errors += $"{error}\n";
+        string errorText = (string)textbox.Clone();
+        int addedIndex = 0;
+        foreach (var error in InputActionParser.Errors)
+        {
+            errors += $"{error}\n";
+        }
     }
 
     ///<inheritdoc/>
     protected static void LoadFont()
     {
-        Texture2D? fontTexture = Assets.GetOrDefault<Texture2D>(":THEFONT.png");
-        string? fontInfo = Assets.GetOrDefault<string>(":THEFONT_info.json");
-        if (fontTexture is not null && fontInfo is not null) GlobalFonts.MenuFont = new BitmapFont(fontTexture, JsonSerializer.Deserialize<BitmapFont.Info>(fontInfo, Json.SerializeWithFields));
+        FontSystem defFontSystem = new();
+        defFontSystem.AddFont(Assets.Get<byte[]>("Fonts/monogram-extended.ttf"));
+        FontSystem italicFontSystem = new();
+        italicFontSystem.AddFont(Assets.Get<byte[]>("Fonts/monogram-extended-italic.ttf"));
+        GlobalFonts.MenuFont = new GenericStashFont(defFontSystem.GetFont(12), null!, italicFontSystem.GetFont(12));
     }
 
     ///<inheritdoc/>
@@ -92,18 +103,19 @@ public class Engine : MonodGame
     /// <inheritdoc/> 
     protected override void DrawM()
     {
-        IFont? font = GlobalFonts.MenuFont;
+        GenericFont? font = GlobalFonts.MenuFont;
         if (font is null) return;
         Renderer.Begin(SpriteSortMode.Immediate, BlendState.NonPremultiplied, SamplerState.PointClamp);
         Renderer.DrawRect(new(0, 0), new(1000, 500), Color.DarkSlateBlue);
         Vector2 pos = offset.ToVector2();
         pos.Y += 50;
         pos.X += 50;
-        font.DrawText(textbox, pos, Color.White, scale: new(2));
+        font.DrawString(Renderer.spriteBatch, text, pos, Color.White);
         pos.Y += 50;
-        font.DrawText(text, pos, Color.White, scale: new(3));
-        pos.Y += 50;
-        font.DrawText(errors, pos, Color.Red, scale: new(2));
+        tokenized.Draw(new(), Renderer.spriteBatch, pos, font, Color.White, 1, 0);
+        font.DrawString(Renderer.spriteBatch, textbox, pos, Color.White);
+        pos.Y += 100;
+        font.DrawString(Renderer.spriteBatch, errors, pos, Color.Red);
         Renderer.End();
     }
 }

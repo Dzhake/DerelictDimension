a2a52a705ff5f10a41d3e60b78720a60525d51cb
Dzhake <andryzindmi@gmail.com>
2026-02-15 23:22:21 +0300

    Finish InputAction parser

--- src/Engine.cs
+++ src/Engine.cs
@@ -28,7 +28,7 @@ public class Engine : MonodGame
 
     public string errors = "";
 
-    public string textbox = "Or(Pressed(D1), And(Down(Ctrl), Pressed(D2)))";
+    public string textbox = "Or(Down(D1), And(Down(LeftControl), Up(D2)))";
 
     public Point offset = Point.Zero;
 
@@ -41,6 +41,7 @@ public class Engine : MonodGame
     /// </summary>
     public Engine()
     {
+        IsMouseVisible = true;
     }
 
     ///<inheritdoc/>
@@ -59,13 +60,27 @@ public class Engine : MonodGame
         MainAssetManager.LoadAssets();
 
         //Action = new OrAction([new HeldAction(Key.D1), new AndAction([new PressedAction(Key.D2), new DownAction(Key.LeftControl)])]);
+        Parse();
+    }
+
+    private void Parse()
+    {
+        errors = "";
         Action = InputActionParser.TryParse(textbox);
-        string errorText = (string)textbox.Clone();
+        string errorText = textbox; //string is recreated after the first Insert
         int addedIndex = 0;
+        string underlineStart = "<u #FF0000>";
+        string underlineEnd = "</u>";
         foreach (var error in InputActionParser.Errors)
         {
+            errorText = errorText.Insert(error.StartIndex + addedIndex, underlineStart);
+            addedIndex += underlineStart.Length;
+            errorText = errorText.Insert(error.StartIndex + error.Length + addedIndex, underlineEnd);
+            addedIndex += underlineEnd.Length;
             errors += $"{error}\n";
         }
+
+        tokenized = new TextFormatter().Tokenize(GlobalFonts.MenuFont!, errorText);
     }
 
     ///<inheritdoc/>
@@ -75,7 +90,7 @@ public class Engine : MonodGame
         defFontSystem.AddFont(Assets.Get<byte[]>("Fonts/monogram-extended.ttf"));
         FontSystem italicFontSystem = new();
         italicFontSystem.AddFont(Assets.Get<byte[]>("Fonts/monogram-extended-italic.ttf"));
-        GlobalFonts.MenuFont = new GenericStashFont(defFontSystem.GetFont(12), null!, italicFontSystem.GetFont(12));
+        GlobalFonts.MenuFont = new GenericStashFont(defFontSystem.GetFont(36), null!, italicFontSystem.GetFont(36));
     }
 
     ///<inheritdoc/>
@@ -90,6 +105,48 @@ public class Engine : MonodGame
             offset.Y -= 1;
         else if (Input.Down(Key.Down))
             offset.Y += 1;
+
+        if (Input.Down(Key.Q))
+        {
+            textbox = "Or(Down(D1), And(Down(LeftControl), Up(D2)))";
+            Parse();
+        }
+        else if (Input.Down(Key.W))
+        {
+            textbox = "Or(Down(D1), And(Down(Mouse99), Up(D2)))";
+            Parse();
+        }
+        else if (Input.Down(Key.E))
+        {
+            textbox = "Or(Down(D1), And(NonExistingAction(LeftControl), Up(D2)))";
+            Parse();
+        }
+        else if (Input.Down(Key.R))
+        {
+            textbox = "Or(Down(D1), And((LeftControl), Up(D2)))";
+            Parse();
+        }
+        else if (Input.Down(Key.T))
+        {
+            textbox = "And(Down(D1), Down(D2))";
+            Parse();
+        }
+        else if (Input.Down(Key.Y))
+        {
+            textbox = "And(oh, no, this, is, wrong!)";
+            Parse();
+        }
+        else if (Input.Down(Key.A))
+        {
+            textbox = "And(oh, no, this, is, wrong!";
+            Parse();
+        }
+        else if (Input.Down(Key.S))
+        {
+            textbox = "And oh, no, this, is, wrong!";
+            Parse();
+        }
+
 
         if (Action?.IsActive(0) ?? false) text = "Active";
         else text = "Inactive";
@@ -106,14 +163,14 @@ public class Engine : MonodGame
         GenericFont? font = GlobalFonts.MenuFont;
         if (font is null) return;
         Renderer.Begin(SpriteSortMode.Immediate, BlendState.NonPremultiplied, SamplerState.PointClamp);
-        Renderer.DrawRect(new(0, 0), new(1000, 500), Color.DarkSlateBlue);
+        Renderer.Clear(Color.Black);
         Vector2 pos = offset.ToVector2();
+        pos.X += 10;
+        font.DrawString(Renderer.spriteBatch, $"D1: {Input.GetValue(Key.D1)}, LeftControl: {Input.GetValue(Key.LeftControl)}, D2: {Input.GetValue(Key.D2)}", pos, Color.White);
         pos.Y += 50;
-        pos.X += 50;
         font.DrawString(Renderer.spriteBatch, text, pos, Color.White);
         pos.Y += 50;
         tokenized.Draw(new(), Renderer.spriteBatch, pos, font, Color.White, 1, 0);
-        font.DrawString(Renderer.spriteBatch, textbox, pos, Color.White);
         pos.Y += 100;
         font.DrawString(Renderer.spriteBatch, errors, pos, Color.Red);
         Renderer.End();
